3dcbc9a71ecf969c9a417af7c0220412c0eee769
Jakub Pelc <jakubpelc86@gmail.com>
2025-11-30 21:39:17 +0100

    setter deleted because its not need

--- Models/ThreadForum.cs
+++ Models/ThreadForum.cs
@@ -3,6 +3,6 @@ public class ThreadForum
     public int Id { get; set; }
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
-    public ICollection<Post> Posts { get; set; } = new List<Post>();
+    public ICollection<Post> Posts { get; } = new List<Post>();
 
 }
