f1e544017bdd254a5a77baf7b90a01b6ca45b30f
Jakub Pelc <jakubpelc86@gmail.com>
2025-11-30 21:43:20 +0100

    created dbsets Threads, Posts, Comments

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -4,5 +4,9 @@ public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
 
+    public DbSet<ThreadForum> Threads { get; set; }
+    public DbSet<Post> Posts { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+
     
 }
