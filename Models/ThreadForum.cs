381ab67c640a74f48bd19b3912d48b3d06d54f52
Jakub Pelc <jakubpelc86@gmail.com>
2025-12-09 21:28:02 +0100

    Basic CreateThread done

--- Models/ThreadForum.cs
+++ Models/ThreadForum.cs
@@ -6,7 +6,7 @@ public class ThreadForum
     public string AuthorId { get; set; }
     public User Author { get; set; } = null!;
     public string Title { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
+    public string? Description { get; set; }
     public ICollection<Post> Posts { get; } = new List<Post>();
 
 }
---
+++ Views/Home/CreateThread.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyForum.Views.Home
+{
+    public class CreateThreadModel
+    {
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(40, MinimumLength = 4, ErrorMessage = "Maximum length is 40, minimum length is 4")]
+        public string Title { get; set; }
+        [StringLength(100, MinimumLength = 4, ErrorMessage = "Maximum length is 100, minimum length is 4")]
+        public string? Description { get; set; }
+
+    }
+}
