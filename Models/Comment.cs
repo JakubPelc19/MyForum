ee5a8a72f1ef53f5d653dc72d1c674218b198f7a
Jakub Pelc <jakubpelc86@gmail.com>
2025-12-09 21:42:41 +0100

    string.Empty deleted from all data models

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -7,6 +7,6 @@ public class Comment
     public Post Post { get; set; } = null!;
     public string AuthorId { get; set; }
     public User Author { get; set; } = null!;
-    public string Content { get; set; } = string.Empty;
+    public string Content { get; set; }
     public int Likes { get; set; }
 }
