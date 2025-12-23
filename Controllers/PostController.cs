517ff0526b15fe14fbf17f39943da40932f36697
Jakub Pelc <jakubpelc86@gmail.com>
2025-12-23 15:12:30 +0100

    PostController/Index basic implementation

---
+++ Controllers/PostController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyForum.Controllers
+{
+    [Route("Thread/{threadId:int}/[controller]")]
+    public class PostController(AppDbContext _context) : Controller
+    {
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> Index(int id)
+        {
+            var post = await _context.Posts.FindAsync(id);
+
+            if (post is null)
+                return NotFound();
+
+            var comments = post.Comments;
+            
+            return View(comments);
+        }
+    }
+}
